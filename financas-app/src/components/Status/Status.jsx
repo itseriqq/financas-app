0270c0fec826214b90702d8666717c3d0310721d
Erick <contato.bioerick@gmail.com>
2024-04-17 20:53:59 -0300

    .

--- financas-app/src/components/Status/Status.jsx
+++ financas-app/src/components/Status/Status.jsx
@@ -1,10 +1,8 @@
 import '@/app/globals.css'
 import styles from './status.module.css'
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { FiTrendingUp } from "react-icons/fi";
 
 import React from 'react'
 
@@ -22,11 +20,15 @@ const Status = ({ titulo, valor, Icone }) => {
             </div>
           </Col>
         </Row>
-        <Row className='text-center mt-2'>
-          <Col md={9} styles={{marginRight: '30px'}}>
-            <h2>{valor}</h2>
+        <Row className='mt-2 text-center'>
+          <Col md={12}>
+            <div>
+              <h2>{valor}</h2>
+            </div>
           </Col>
-          <Col md={3} styles={{marginLeft: '10px'}}>
+        </Row>
+        <Row className='text-center'>
+          <Col md={12}>
 
             {Icone && <Icone />}
 
