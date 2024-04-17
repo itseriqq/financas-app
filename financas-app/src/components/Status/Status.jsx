f3a209c83e4128d9702695d7370c703add312d0f
erick. <124532277+itseriqq@users.noreply.github.com>
2024-04-17 21:44:42 +0000

    Added EditarItem and RemoveItem buttons

--- financas-app/src/components/Status/Status.jsx
+++ financas-app/src/components/Status/Status.jsx
@@ -1,5 +1,5 @@
 import '@/app/globals.css'
-import styles from './status.module.css'
+import styles from './Status.module.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
